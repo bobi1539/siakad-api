a7e0bff211dd4e02bb285244be094b9171305a34
bobi.rival <bahmadrival@gmail.com>
2025-06-13 15:20:33 +0700

    Init Project

---
+++ src/main/java/com/siakad/SiakadApiApplication.java
@@ -0,0 +1,13 @@
+package com.siakad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiakadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiakadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siakad/SiakadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siakad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiakadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
